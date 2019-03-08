78016ffbe1ee796bbbfde935a686e91ff4edfcd7
RabbitGamer <jdakers1997@gmail.com>
2019-03-08 12:00:38 +0000

    Completed upto Task 8 on the UFOTutorial

---
+++ UnityUFOTutorial/Assets/_Completed-Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb2d;
+    private int count;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+        count = 0;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
+    }
+}
